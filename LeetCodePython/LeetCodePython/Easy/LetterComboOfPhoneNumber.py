71847b3e6384cd5c27710b23fa018873a59bdc48
Ryan Way <ryan.keith.way@gmail.com>
2019-05-08 23:39:28 -0700

    solved, also added very pythony solution

---
+++ LeetCodePython/LeetCodePython/Easy/LetterComboOfPhoneNumber.py
@@ -0,0 +1,50 @@
+class Solution:
+    
+    numToLetters = [
+            [], #dummy value
+            [],
+            ["a", "b", "c"],
+            ["d", "e", "f"],
+            ["g", "h", "i"],
+            ["j", "k", "l"],
+            ["m", "n", "o"],
+            ["p", "q", "r", "s"],
+            ["t", "u", "v"],
+            ["w", "x", "y", "z"],
+            []
+        ]
+
+    def letterCombinations(self, digits: str):
+        if len(digits) == 0: return []
+        if len(digits) == 1: return self.numToLetters[int(digits[0])]
+        
+        retVal = []
+        for i in reversed(digits):
+            if len(retVal) == 0:
+                retVal = self.numToLetters[int(i)]
+            else:
+                list = []
+                for j in self.numToLetters[int(i)]:
+                    for k in retVal:
+                        list.append(j + k)
+                retVal = list
+        return retVal
+
+    #very pythony version
+    def letterCombo_pythony(self, digits: str):
+        if len(digits) == 0: return []
+        if len(digits) == 1: return self.numToLetters[int(digits[0])]
+        
+        retVal = []
+        for i in reversed(digits):
+            if len(retVal) == 0:
+                retVal = self.numToLetters[int(i)]
+            else:
+                a = self.numToLetters[int(i)]
+                a, retVal = [a[i//len(retVal)] for i in range(len(a)*len(retVal))], retVal*len(a)
+                retVal = [i+j for i,j in zip(a,retVal)]
+        return retVal
+
+
+    
+
---
+++ LeetCodePython/LeetCodePythonTest/EasyTest/LetterComboOfPhoneNumberTest.py
@@ -0,0 +1,30 @@
+import unittest
+from LetterComboOfPhoneNumber import Solution
+
+class Test_LetterComboOfPhoneNumberTest(unittest.TestCase):
+    
+    def test_SingleDigit(self):
+        digits = "9"
+        solution = Solution()
+        actual = solution.letterCombinations(digits)
+        exp = ["w", "x", "y", "z"]
+        self.assertEqual(actual, exp)
+
+    def test_Example(self):
+        digits = "23"
+        solution = Solution()
+        actual = solution.letterCombinations(digits)
+        exp = ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
+
+        self.assertEqual(actual, exp)
+
+    def test_Empty(self):
+        digits = ""
+        solution = Solution()
+        actual = solution.letterCombinations(digits)
+        exp = []
+
+        self.assertEqual(actual, exp)
+
+if __name__ == '__main__':
+    unittest.main()
